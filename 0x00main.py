1a4e537a978a434b6fbd4d1c1869743ab014ffef
JULIA-FRANCES <juliafranceskosisochukwu2@gmail.com>
2026-07-04 21:40:02 +0100

    Fix broken script with correct comparison and indentation

---
+++ 0x00main.py
@@ -0,0 +1,15 @@
+#Exercise 1 - Fix the Broken Script
+#Broken Script:
+
+#name = "Bob"
+#if name = "Bob"
+#print("Hello, Bob!")
+#else:
+#print("I don't know you.")
+
+name ="Bob"
+if name == "Bob":
+   print("Hello, Bob!")
+else:
+   print("I don't know you.")
+
