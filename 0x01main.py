3d261c2ecb57d7137e33b78e2b111dafbad50857
JULIA-FRANCES <juliafranceskosisochukwu2@gmail.com>
2026-07-04 22:02:28 +0100

    Add personal_greet function for exercise 2

---
+++ 0x01main.py
@@ -0,0 +1,10 @@
+#Exercise 2 - Write Your Own Greeting Function
+
+
+def personal_greet(name):
+   
+    print(f"Hello, {name}! Welcome to Python!")
+
+# Calling the function with different names
+personal_greet("Alice")
+personal_greet("Charlie")
